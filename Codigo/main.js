e42e78c0225dfd25254199d5085c527703d03a68
Alvaro Viveiros <arthur.viveiros.n@gmail.com>
2020-02-07 01:45:02 -0300

    Desafio parcialmente terminado

---
+++ Codigo/main.js
@@ -0,0 +1,9 @@
+let valoratual = document.getElementById("valor");
+console.log(valoratual);
+
+
+function colocanocarrinho() {
+    valoratual = valoratual + 1;
+    document.getElementById("valor").innerHTML = valoratual;
+    console.log(valornovo);
+}
